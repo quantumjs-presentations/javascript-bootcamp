31d2b3e92fb3bf5d3a99bda0fe6b9a59b43fe789
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-02-07 22:36:42 +0000

    More type studies

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # javascript-bootcamp
 
 Learning JS by example
+
+# Practice slow, to learn fast "A sensai"
+
+Strong foundations mean we can go further for longer
--- types.js
+++ types.js
@@ -1,24 +1,52 @@
 //variable declaration
 var item
-console.log(typeof item)
 
 //numbers
-var item = 10
-console.log(typeof item)
-console.log(item * 2)
-console.log(Math.pow(item, 2))
+item = 10
+item = item * 2
+item = Math.pow(item, 2)
 console.log(item ** 2)
 
 
 //strings
 item = "test"
-console.log(typeof item)
-console.log(item * 2)
+item = item * 2
 console.log(typeof (item * 2))
-console.log(item + item)
-console.log(item.length())
-item = "2"
-console.log(item * 2)
+item = item + item
+item = item.length
+item  = "2"
+item = item.concat(23)
+item = item * 2
+item = item + "three"
+console.log(item * "three")
 
+//booleans and conditionals
+item = false
+item = !item
+item = !!item
+item && console.log("this will never print")
+item = true
+item && console.log("this will always print")
+
+//equalities http://www.ecma-international.org/ecma-262/7.0/index.html#sec-abstract-equality-comparison
+console.log(typeof "")
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(false == "")
+console.log(false == 0)
+console.log(false == null)
+console.log(false == null)
+console.log(false == undefined)
+console.log(false == {})
+console.log(undefined == null)
+
+//strict equality (in general use this unless you have a good reason)
+console.log(false === "")
+console.log(false === 0)
+
+// obejcts
+item = {}
+debugger
+item.toString()
 
 
