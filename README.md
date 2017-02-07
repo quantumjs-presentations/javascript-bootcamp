a1250b3ea3ca36139d7e6ef3ab139d32e730e883
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-02-07 14:26:52 +0000

    Start the types examples

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-bootcamp
+
+Learning JS by example
---
+++ types.js
@@ -0,0 +1,24 @@
+//variable declaration
+var item
+console.log(typeof item)
+
+//numbers
+var item = 10
+console.log(typeof item)
+console.log(item * 2)
+console.log(Math.pow(item, 2))
+console.log(item ** 2)
+
+
+//strings
+item = "test"
+console.log(typeof item)
+console.log(item * 2)
+console.log(typeof (item * 2))
+console.log(item + item)
+console.log(item.length())
+item = "2"
+console.log(item * 2)
+
+
+
